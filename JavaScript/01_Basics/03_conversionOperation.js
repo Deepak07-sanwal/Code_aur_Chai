da4169e070e9b23a30f5aa8a3928fbc4d586c908
Deepak07-sanwal <73419872+Deepak07-sanwal@users.noreply.github.com>
2023-11-08 14:26:09 +0000

    operational

--- JavaScript/01_Basics/03_conversionOperation.js
+++ JavaScript/01_Basics/03_conversionOperation.js
@@ -54,4 +54,42 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+/// **************************** Operations *****************************
 
+let value = 3
+let negValue = -value
+console.log(negValue) // -3
+
+console.log(2+2);
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) // power
+console.log(4/2)
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Deepak"
+let str3 = str1 + str2 ;
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+
+console.log(3 + 4 * 5 % 3) // not a good coding practise in practical world
+
+
+console.log(true); // true
+console.log(+true); // 1
+// console.log(true+); // error
+console.log(+"")  // 0
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2  
+
+let gameCounter = 100 ;
+gameCounter++ ; // postfix
+console.log(gameCounter);
+++gameCounter ; // prefix
+console.log(gameCounter)
