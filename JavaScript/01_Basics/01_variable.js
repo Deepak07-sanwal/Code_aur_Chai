54844fdb5df15c9779aebb21b106617a780985f8
Deepak07-sanwal <73419872+Deepak07-sanwal@users.noreply.github.com>
2023-11-08 11:24:15 +0000

    01_variable

---
+++ JavaScript/01_Basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553 // can't be changed
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 123 // error can not be changed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId) ;
+console.table([accountId,accountEmail,accountPassword,accountCity])
