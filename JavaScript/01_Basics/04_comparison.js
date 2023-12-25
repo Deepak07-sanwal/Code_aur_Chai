72c1eda591dd003ae762d652ad76fe2bfaa2cce5
Deepak07-sanwal <deepaksanwal077@gmail.com>
2023-12-25 17:30:10 +0530

    comparison continue

--- JavaScript/01_Basics/04_comparison.js
+++ JavaScript/01_Basics/04_comparison.js
@@ -5,12 +5,12 @@ console.log(2 == 1);
 console.log(2 != 1);
 
 // don't compare two different datatype -> error in typescript
-console.log("2" > 1)
-console.log("02" > 1) 
+console.log("2" > 1) // true
+console.log("02" > 1) // true -> string convert into number
 // typescript give some rules for this as comapare to javascript
 
 console.log(null > 0) // false
-console.log(null == 0) // false when NaN convert or true when 0 convert
+console.log(null == 0) // false when null is converted to NaN or true when converted to 0 
 console.log(null >= 0) // true
 
 // The reason is that an equality check == and comparisions > < >= <= work differently.
